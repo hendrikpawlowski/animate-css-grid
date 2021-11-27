5d56319655534a99612b59d201e4f1afb8e99ea5
Hendrik Pawlowski <hendrik@Air-von-Hendrik.fritz.box>
2021-11-27 13:39:17 +0100

    added npm

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        violet: colors.violet,
+        fuchsia: colors.fuchsia,
+        lightBlue: colors.lightBlue,
+        cyan: colors.cyan,
+        teal: colors.teal,
+        emerald: colors.emerald,
+        lime: colors.lime,
+        amber: colors.amber,
+        warmGray: colors.warmGray,
+        trueGray: colors.trueGray,
+        coolGray: colors.coolGray,
+        blueGray: colors.blueGray,
+
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
